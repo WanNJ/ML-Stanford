75c50a4a1c48296cf532850fb43493449dd89b06
WanNJ <omyjackwan@gmail.com>
2017-11-25 23:13:44 -0800

    Finish Normalization(Feature Scaling)

--- EX1/ex1/ex1_multi.m
+++ EX1/ex1/ex1_multi.m
@@ -49,10 +49,10 @@ pause;
 % Scale features and set them to zero mean
 fprintf('Normalizing Features ...\n');
 
-[X mu sigma] = featureNormalize(X);
+[X, mu, sigma] = featureNormalize(X);
 
 % Add intercept term to X
-X = [ones(m, 1) X];
+X = [ones(m, 1), X];
 
 
 %% ================ Part 2: Gradient Descent ================

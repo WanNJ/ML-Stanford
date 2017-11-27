4a370f386be3584116ef6e1b813d9842563afc87
WanNJ <omyjackwan@gmail.com>
2017-11-26 16:12:26 -0800

    Finish One-Vs-All prediction

--- EX3-NeuralNetwork/ex3/predictOneVsAll.m
+++ EX3-NeuralNetwork/ex3/predictOneVsAll.m
@@ -6,13 +6,13 @@ function p = predictOneVsAll(all_theta, X)
 %  rows. all_theta is a matrix where the i-th row is a trained logistic
 %  regression theta vector for the i-th class. You should set p to a vector
 %  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
-%  for 4 examples) 
+%  for 4 examples)
 
 m = size(X, 1);
-num_labels = size(all_theta, 1);
+num_labels = size(all_theta, 1); %#ok<*NASGU>
 
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+p = zeros(size(X, 1), 1); %#ok<*PREALL>
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
@@ -28,15 +28,12 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
+%
 
+h = sigmoid(X * all_theta');
 
-
-
-
-
+[~, p] = max(h, [], 2);
 
 % =========================================================================
-
 
 end

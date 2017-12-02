3882918d83806f012fd3e87eec0052a9abf2b981
WanNJ <omyjackwan@gmail.com>
2017-12-01 22:24:00 -0800

    Finish PCA Projection

--- EX7-KMeans&PCA/ex7/projectData.m
+++ EX7-KMeans&PCA/ex7/projectData.m
@@ -6,8 +6,6 @@ function Z = projectData(X, U, K)
 %   the first K columns of U. It returns the projected examples in Z.
 %
 
-% You need to return the following variables correctly.
-Z = zeros(size(X, 1), K);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the projection of the data using only the top K 
@@ -18,7 +16,9 @@ Z = zeros(size(X, 1), K);
 %                    projection_k = x' * U(:, k);
 %
 
+U_reduce = U(:, 1:K);
 
+Z = X * U_reduce;
 
 
 % =============================================================

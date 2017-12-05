95bc601e091901ee08703777790509b6ba2aa53d
WanNJ <omyjackwan@gmail.com>
2017-12-05 14:17:19 -0800

    Finish Regularized Gradient of Collaborative Filtering Algorithm

--- EX8-AnomalyDetection&RecommenderSystem/ex8/cofiCostFunc.m
+++ EX8-AnomalyDetection&RecommenderSystem/ex8/cofiCostFunc.m
@@ -41,20 +41,20 @@ Theta_grad = zeros(size(Theta));
 %
 
 
-J = sum(sum(((X * Theta' - Y) .* R) .^2))/2;
+J = sum(sum(((X * Theta' - Y) .* R) .^2))/2 + lambda/2*(sum(sum(Theta .^ 2)) + sum(sum(X .^ 2)));
 
 for i = 1:num_movies
     idx = find(R(i, :)==1);
     Thetatemp = Theta(idx, :);
     Ytemp = Y(i, idx);
-    X_grad(i, :) = (X(i, :) * Thetatemp' - Ytemp) * Thetatemp;
+    X_grad(i, :) = (X(i, :) * Thetatemp' - Ytemp) * Thetatemp + lambda * X(i, :);
 end
 
 for j = 1:num_users
     idx = find(R(:, j)==1);
     Xtemp = X(idx, :);
     Ytemp = Y(idx, j);
-    Theta_grad(j, :) = Xtemp' * (Xtemp * Theta(j, :)' - Ytemp);
+    Theta_grad(j, :) = (Xtemp * Theta(j, :)' - Ytemp)' * Xtemp + lambda * Theta(j, :);
 end
 
 % =============================================================
